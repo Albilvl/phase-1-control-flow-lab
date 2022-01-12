66354bd59b1ffa819062237e7a3c3a10c3f66ae8
Al Bilal <bil.bank@yahoo.com>
2022-01-12 16:41:34 -0500

    commit tree 2c453a07e67c80caf7cd7344b78a9ddc80247e0b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    if (feet > 2500){
+        return (`No can do.`)
+  }   else if (feet > 2000){
+        return 'I will gladly take your thirty bucks.'
+  } else {
+        return `This one is on me!`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const ten = city === 'NYC' ? ("Ok, sounds good.") : ("No go.")
+  return ten
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === `generous`){
+    return "Thank you so much."
+  } else if (tip === `not as generous`){
+    return "Thank you."
+  } else {
+    return `Bye.`
+  }
+
 }
